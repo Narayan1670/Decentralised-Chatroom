a7eae608f7e5152ccdb594a5efb2047ac9a0c519
Vishnu <vishnunarayan1670@gmail.com>
2022-09-02 21:06:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Serverless Chat App
+
+A decentralized chat app built with GUN
+
+- Try the [Live Demo](https://decentralised-app-6f930.web.app/)
+
+
+```
+git clone <this-repo>
+npm install
+npm run dev
+```
---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>💬</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>Serverless Chat</h3>
+  {/if}
+</header>
